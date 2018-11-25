744718c1098fe95280b07b92589e5c74536c5ed1
Joohoon Kim <joohoonkim@dyn129-100-133-172.eduroam.uwo.ca>
2018-11-25 09:31:02 -0500

    finalized layout

--- YourTrash/app/src/main/java/com/hackwestern5/yourtrash/yourtrash/Results.java
+++ YourTrash/app/src/main/java/com/hackwestern5/yourtrash/yourtrash/Results.java
@@ -33,6 +33,10 @@ public class Results extends AppCompatActivity {
         String location = intent.getStringExtra(StartUp.LOCATION);
         TextView locationBox = findViewById(R.id.databox_location);
         locationBox.setText(location);
+
+        String final_res = returnExplanation(location, object_name);
+        TextView resultsBox = findViewById(R.id.databox_results);
+        resultsBox.setText(final_res);
     }
 
     private String returnObjectName(String object){
@@ -65,16 +69,64 @@ public class Results extends AppCompatActivity {
             String[] objbuff = tempArray[index].split(delimiter);
             String obj = objbuff[0].trim();
 
-            if (obj.equals("material") || obj.equals("product") || obj.equals("wool")) {
+            if (obj.equals("material") || obj.equals("product") || obj.equals("wool") || obj.equals("hardware")) {
                 objbuff = tempArray[index_2].split(delimiter);
                 obj = objbuff[0].trim();
             }
 
-            if (obj.equals("water")) {
-                return "water bottle";
+            if (obj.equals("water") || obj.equals("bottle")) {
+                return "plastic bottle";
             }
+
+            if (obj.equals("cylinder")){
+                return "aluminum can";
+            }
+
             return obj;
         }
         return out;
+    }
+
+    private String returnExplanation(String location, String object){
+        String results = "";
+        if (location.equals("London")) {
+            if (object.equals("plastic bottle") || object.equals("plastic")) {
+                results = "London: Please place all your plastic containers in your Containers Bin.";
+            } else if (object.equals("paper towel") || object.equals("paper")) {
+                results = "London: Please dispose of any paper towels and napkins in your Garbage Bin.";
+            } else if (object.equals("aluminum can")) {
+                results = "London: Please place this item in your Blue Bin.  If appropriate, please empty and rinse this item.";
+            } else if (object.equals("electronic device") || object.equals("laptop") || object.equals("technology") || object.equals("wire")) {
+                results = "London: Please dispose all electronics at your local City of London Drop-off Depot.";
+            } else if (object.equals("fruit") || object.equals("banana")){
+                results = "London: Please place this item in your Green Bin.";
+            } else {
+                results = "The material looks like garbage please dispose of it appropriately.";
+            }
+        }
+        else if (location.equals("Toronto")) {
+            if (object.equals("plastic bottle")) {
+                results = "Toronto: Please place empty containers in your Blue Bin, leave lids and sprayers on.";
+            }
+            else if (object.equals("paper towel") || object.equals("paper")) {
+                results = "Toronto: Please place this item in your Green Bin.";
+            }
+            else if (object.equals("aluminum can")) {
+                results = "Toronto: Please place this item in your Blue Bin.  If appropriate, please empty and rinse this item.";
+            }
+            else if (object.equals("electronic device") || object.equals("laptop") || object.equals("technology") || object.equals("wire") ||
+                    object.equals("netbook") || object.equals("audio equipment") || object.equals("headphones")){
+                results = "Toronto: Please dispose all electronics  at your local City of Toronto Drop-off Depot";
+            }
+            else if (object.equals("fruit") || object.equals("banana")) {
+                results = "Toronto: Please place this item in your Green Bin.";
+            } else {
+                results = "The material looks like garbage please dispose of it appropriately.";
+            }
+        }
+        else {
+            results = "Sorry, London and Toronto are currenty the only supported cities.";
+        }
+        return results;
     }
 }
